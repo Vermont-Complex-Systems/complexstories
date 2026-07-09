32998c1d33d51d37d239be39465ddc324b34d64a
Ben Cooley <bcooley@uvm.edu>
2026-07-09 11:43:34 -0400

    changes from sam

--- src/lib/stories/papers-reviewed/components/Index.svelte
+++ src/lib/stories/papers-reviewed/components/Index.svelte
@@ -3,6 +3,8 @@
 	import BackToHome from '$lib/components/helpers/BackToHome.svelte';
 	import BlackBox from './BlackBox.svelte';
 	import SankeyDiagram from './SankeyDiagram.svelte';
+	import fig1Image from '$lib/stories/papers-reviewed/data/Fig1Y.png';
+	import sciHumlogo from '$lib/stories/papers-reviewed/data/sci-hum-lab-logo.png';
 
 	let { story: _story, data } = $props();
 
@@ -12,9 +14,50 @@
 
 <BackToHome />
 
+<style>
+	.lab-credit {
+		text-align: center;
+		margin: 0rem auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 2rem;
+	}
+
+	.story h1 {
+		max-width: 1000px;
+		margin: 0 auto;
+		text-align:center;
+		margin-bottom: 0.5rem;
+	}
+
+	.subtitle {
+		max-width: 700px;
+		font-size: 1.5rem;
+		margin: 0 auto;
+		text-align:center;
+		margin-bottom: 1rem;
+	}
+</style>
+
 <article class="story">
 	<h1>{data.title}</h1>
-	<p>{data.subtitle}</p>
+	<p class="subtitle">{data.subtitle}</p>
+	<div class="lab-credit">
+		<p>A project by:</p>
+		<a href="https://scienceandhumanitylab.github.io/" target="_blank" rel="noopener noreferrer">
+		<img src={sciHumlogo} width="80" alt="Science and Humanity Lab Logo" />
+		</a>
+	</div>
+
+	<section>
+		<p>
+			Elite general science journals shape scientific discourse, public policy, and scientific careers. Publishing a manuscript in one of these journals can alter the direction not just of an individual career, but of an entire scientific field. Therefore, scientists have a vested interest in understanding the editorial processes shaping outcomes at these journals. However, scientific journals also have a long tradition of reviewer confidentiality, making it difficult to allow the broader scientific community a closer look.
+		</p>
+		<p>
+			Among the most prestigious and impactful journals are Science and Science Advances, two general science journals published by the American Association for the Advancement of Science (AAAS). With collaborators, director of VCSI’s Science and Humanity Lab Sam Zhang worked with AAAS to de-identify their editorial data, and publish a  dataset of the editorial outcomes of 110,303 manuscripts submitted between 2015-2020 for the two journals [eventual citation to paper here?]. This dataset allows the scientific community to understand elite peer review as a complex system.
+		</p>
+	</section>
 
 	<section class="fullscreen-layout">
 		<div class="sticky-panel">
@@ -26,9 +69,9 @@
 	</section>
 
 	<section>
-		<p>Here is some text to act as a transition between sections.</p>
-		<p>We can explain some things about the story.</p>
-		<p>And introduce the sankey diagram.</p>
+		<p>The newly released AAAS data allows the scientific community to quantify the editorial dynamics of submissions to Science (and Science Advances) over a five year period. Most submissions are rejected, and there are multiple stages where rejection can occur. A successful submission must clear several hurdles by multiple sets of evaluators.</p>
+		<p>First, the editor rejects certain unsuitable submissions immediately. The submissions not immediately rejected are typically routed to a small group of several hundred external experts, called the Board of Reviewing Editors (BoRE), who quickly evaluate the manuscript and provide a numeric score and suggested reviewers. BoRE feedback and scores is not made available to authors, but it is present in the new dataset.</p>
+		<p>Based on BoRE feedback, the editor rejects another set of submissions. The remaining manuscripts are forwarded to peer reviewers. The editor makes the decision whether to proceed to subsequent rounds of review, acceptance, or rejection, based on the comments from the peer reviewers.</p>
 	</section>
 
 	<section class="fullscreen-layout">
@@ -38,6 +81,16 @@
 		<div class="scrolly-content">
 			<ScrollyContent steps={data.steps} bind:value={scrollyIndex} />
 		</div>
+	</section>
+
+	<section>
+		<p>
+			This dataset provides the community an unprecedented opportunity to glimpse into the inner workings of peer review, and to quantify the correlates of outcome disparities. The team found higher acceptance rates among larger teams whose corresponding authors worked in the US, Canada, or Europe, and those at prestigious institutions, as well as a small gender disparity (Fig. 1). However, these results are not causal, and there can be unmeasured attributes that explain these disparities.
+		</p>
+		<img src={fig1Image} alt="Figure 1: Acceptance rates by author gender, team size, and region" />
+		<p>
+			Randomized controlled experiments at the journals, such as through double-blinding, can help measure the causal effect of bias compared to other unmeasured correlates such as the manuscript’s novelty or quality. The release of this data by the AAAS serves as a welcome example for the scientific community, and the collaboration with academic institutions provides a template for how other academic journals can demonstrate their public commitment to transparency.
+		</p>
 	</section>
 </article>
 
